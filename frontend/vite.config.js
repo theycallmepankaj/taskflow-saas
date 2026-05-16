b9fa44a0981d0884085523eb87fc99109cf7a313
pankaj123-rathore <pankajrathore05282@gmail.com>
2026-05-16 23:08:46 +0530

    Fix Railway allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  
+  server: {
+    host: '0.0.0.0',
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'intuitive-love-production-4355.up.railway.app'
+    ]
+  }
 })
